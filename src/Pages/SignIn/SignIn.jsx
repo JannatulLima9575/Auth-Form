e0d26b91555b8b64212c52967c8c77aee64166bf
Jannatul <limajannat1675@gmail.com>
2025-05-01 13:44:14 +0600

    added logic in sign in page

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -1,11 +1,33 @@
+import { signInWithEmailAndPassword } from 'firebase/auth/web-extension';
 import React from 'react';
+import { NavLink } from 'react-router';
+import { auth } from './../../Firebase/Firebase.config';
 
 const SignIn = () => {
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const email = e.target.email.value;
+		const password = e.target.password.value;
+		console.log(email, password);
+
+	signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+    	const user = userCredential.user;
+		console.log(user);
+		
+  	})
+  	.catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
     return (
         <div className="w-full max-w-md p-4 mx-auto rounded-md shadow sm:p-8 mt-[100px] mb-[100px]">
-	<h2 className="mb-3 text-3xl font-semibold text-center">Login to your account</h2>
+	<h2 className="mb-3 text-3xl font-semibold text-center">Sign to your account</h2>
 	<p className="text-sm text-center dark:text-gray-600">Dont have account?
-		<a href="#" rel="noopener noreferrer" className="focus:underline hover:underline">Sign up here</a>
+		<NavLink to="/signup" className="focus:underline hover:underline">Sign up here</NavLink>
 	</p>
 	<div className="my-6 space-y-4">
 		<button aria-label="Login with Google" type="button" className="flex items-center justify-center w-full p-4 space-x-4 border rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-600 focus:dark:ring-violet-600">
@@ -32,7 +54,7 @@ const SignIn = () => {
 		<p className="px-3 dark:text-gray-600">OR</p>
 		<hr  className="w-full dark:text-gray-600" />
 	</div>
-	<form noValidate="" action="" className="space-y-8">
+	<form onSubmit={handleSubmit} noValidate="" action="" className="space-y-8">
 		<div className="space-y-4">
 			<div className="space-y-2">
 				<label htmlFor="email" className="block text-sm">Email address</label>
@@ -46,7 +68,7 @@ const SignIn = () => {
 				<input type="password" name="password" id="password" placeholder="*****" className="w-full px-3 py-2 border rounded-md dark:border-gray-300 dark:bg-gray-50 dark:text-gray-800 focus:dark:border-violet-600" />
 			</div>
 		</div>
-		<button type="button" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">Sign in</button>
+		<button type="submit" className="w-full px-8 py-3 font-semibold rounded-md dark:bg-violet-600 dark:text-gray-50">Sign in</button>
 	</form>
 </div>
     );
